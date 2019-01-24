38207c3050e07572bc29ec2f5ebbf56298559352
HeiddisAnna <heiddisanna92@gmail.com>
2019-01-24 11:24:04 +0000

    verkefni1 vefforritun2

---
+++ app.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const express = require('express');
+const lectures = require('./lectures');
+
+//Skilgreina hvaða js skrá á að nota
+const content = require('./content');
+
+const app = express();
+
+
+app.locals.setContent = content;
+//app.locals.foo = bar => `foo ${bar}`;
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', lectures);
+
+
+//Villumeðhöndlun
+//404 villa 
+function notFoundHandler(req, res, next) {
+    const title = 'Síða fannst ekki';
+    const message = 'Efnið fannst ekki';
+    res.status(404).render('error', {title, message});
+}
+
+//500 villa
+function errorHandler(req, res, next) {
+    const title = 'Villa';
+    const message = 'Það kom upp villa við keyrslu';
+    res.status(500).render('error', {title, message});
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+    console.info(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
---
+++ lectures.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const createContent = require('./content');
+const router = express.Router();
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+async function readList() {
+  const data = await readFile('./lectures.json');
+  const json = JSON.parse(data);
+  return json;
+}
+
+function catchErrors(fn) {
+  return (req, res, next) => fn(req, res, next).catch(next);
+}
+
+async function list(req, res) {
+  const title = 'Fyrirlestrar';
+  const underTitle = 'Vefforritun';
+  const data = await readList();
+  const { lectures } = data;
+
+  res.render('lectures', { title, underTitle, lectures, frontPages: true });
+}
+
+async function lecture(req, res, next) {
+  const { slug } = req.params;
+
+  const data = await readList();
+  const { lectures } = data;
+  const rightLecture = lectures.find(a => a.slug === slug);
+
+  if(!rightLecture){
+    return next;
+  }
+
+  //const lecture = rightLecture.content;
+
+  res.render('lecture', { title: rightLecture.title, underTitle: rightLecture.category,
+     lecture: rightLecture, frontPages: false } );
+}
+
+router.get('/', catchErrors(list));
+router.get('/:slug', catchErrors(lecture));
+
+module.exports = router;
